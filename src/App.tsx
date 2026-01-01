8352ae99c52b45c362f34473484cd67098d4c6ce
Mansour Mbengue <mbgmansour13@gmail.com>
2026-01-01 15:54:46 +0100

    add main application structure with Navbar and section components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Navbar } from '@/layout/Navbar'
+import { Hero } from '@/sections/Hero'
+import { About } from '@/sections/About'
+import { Projects } from '@/sections/Projects'
+import { Contact } from '@/sections/Contact'
 
 function App() {
 
   return (
-    <>
-      Hi
-    </>
+    <div className='min-h-screen overflow-x-hidden'>
+      <Navbar/>
+      <main>
+        <Hero/>
+        <About/>
+        <Projects/>
+        <Contact/>
+      </main>
+    </div>
   )
 }
 
